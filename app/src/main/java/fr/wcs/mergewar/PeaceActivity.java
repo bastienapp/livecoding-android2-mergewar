fb51a292ec158aeb6e568f6ef6272f67e48065c8
Thomas Patérac <paterac.thomas@gmail.com>
2018-03-14 14:43:44 +0100

    peacezone2

---
+++ app/src/main/java/fr/wcs/mergewar/PeaceActivity.java
@@ -0,0 +1,13 @@
+package fr.wcs.mergewar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PeaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peace);
+    }
+}
